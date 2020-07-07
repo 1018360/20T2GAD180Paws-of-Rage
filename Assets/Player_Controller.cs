24048330180d15dae97167b0abba6d8650998fcf
tim pearson <1018360@student.sae.edu.au>
2020-07-07 11:52:20 +1000

    Movement yay
    the box i made moves up and down in lanes

---
+++ Assets/Player_Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private Vector2 targetPos;
+    public float Yincrement;
+    public float speed;
+    public float MaxHeight;
+    public float MinHeight;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < MaxHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > MinHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+            
+        }
+    }
+}
