419c4f433e8e479e4912e96b3c8d377cf751a0ba
Nora <1018949@student.sae.edu.au>
2020-07-27 12:47:00 +1000

    Parallax Background
    Implemented the parallax background, with a script to govern its behaviour, using a sprite I made in Krita

---
+++ Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    public float speed;
+    public float endX;
+    public float startX;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        if (transform.position.x <= endX)
+        {
+            Vector3 pos = new Vector3(startX, transform.position.y, transform.position.z);
+            transform.position = pos;
+        }
+    }
+}
